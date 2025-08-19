1225ca1b350128f74da90dd62a60c03b7e65e9dc
NaturalCapsule <vescxve@gmail.com>
2025-08-19 22:13:17 +0300

    test

--- check_version.py
+++ check_version.py
@@ -2,3 +2,21 @@ import requests
 
 from packaging import version
 
+APP_VERSION = "1.0"
+VERSION_URL = 'https://github.com/NaturalCapsule/IDE/blob/main/version/version.json'
+
+def checkUpdate():
+    # try:
+    response = requests.get(VERSION_URL, timeout = 5)
+    data = response.json()
+    latest = data['Version']
+    url = data['URL']
+    if version.parse(latest) > version.parse(APP_VERSION):
+        print("UPDATE KYYPTO")
+    else:
+        print("Up to date!")
+    # except Exception as e:
+        # print(e)
+
+
+checkUpdate()
